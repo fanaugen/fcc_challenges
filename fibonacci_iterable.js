8037f54dd68518764443a0161cc4952cdc2b7482
Alex Jahraus <alexanderjahraus@notonthehighstreet.com>
2016-07-30 18:50:22 +0200

    Sum odd fibonacci numbers smaller than N
    Now ES5 compliant because I’m too stupid to use node

--- fibonacci_iterable.js
+++ fibonacci_iterable.js
@@ -1,18 +1,42 @@
 function makeFibonacciIterator() {
-  let [prev, curr] = [0, 1];
-  
+  var prev = 0,
+      curr = 1,
+      fibo;
+
   return {
     next: function() {
-      [prev, curr] = [curr, prev + curr];
+      fibo = prev + curr;
+      prev = curr;
+      curr = fibo;
       return prev;
     }
-  };    
+  };
 }
 
-var fibMaker = makeFibonacciIterator();
+function makeOddFibonacciIterator() {
+  var fibonacci = makeFibonacciIterator();
 
-// print the first 20 Fibonacci numbers
-console.clear();
-for (let fib of (new Array(20))) {
-  console.log(fibMaker.next().value);
+  return {
+    next: function() {
+      var fib;
+      while(true) {
+        fib = fibonacci.next();
+        if (fib % 2 > 0) return fib; // number is odd
+      }
+    }
+  }
 }
+
+function sumFibs(upperBound) {
+  var oddFib = makeOddFibonacciIterator(),
+      sum    = 0,
+      fib;
+
+  while (true) {
+    fib = oddFib.next();
+    if (fib > upperBound) return sum;
+    sum += fib;
+  }
+}
+
+console.log(sumFibs(4000000)); // should return 4613732
